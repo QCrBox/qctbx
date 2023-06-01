cc1be283464acc497cdcf7a67f3bd304b9944dd0
Paul Niklas Ruth <pruth@uni-goettingen.de>
2023-06-01 17:47:12 +0100

    Put everything in the scaff subdirectory Implemented HortonPartitioner

---
+++ scaff/LCAODensityCalculators/ORCADensityCalculator.py
@@ -0,0 +1,251 @@
+from .LCAODensityCalculatorBase import LCAODensityCalculator
+from ..util import batched
+import platform
+import shutil
+import pathlib
+import subprocess
+from typing import Dict, List, Union, Optional
+
+calc_defaults = {
+    'filebase': 'orca',
+    'output_format': 'mkl'
+}
+
+qm_defaults = {
+    'method': 'PBE',
+    'basis_set': 'def2-SVP',
+    'multiplicity': 1,
+    'charge': 0,
+    'keywords': [],
+    'blocks': {}
+}
+
+class ORCADensityCalculator(LCAODensityCalculator):
+    """
+    A specialized calculator for using the ORCA quantum chemistry package that inherits from LCAODensityCalculator. 
+    This class provides methods to generate input files, execute ORCA, and process the output.
+
+    Attributes:
+        xyz_format (str): The format of the atomic coordinates.
+        provides_output (tuple): The output formats supported by the calculator.
+        atom_site_required (tuple): The required attributes for the atom_site_dict parameter.
+        qm_options (Dict[str, Any]): Quantum mechanics options for the ORCA calculation.
+            Keys:
+                'method': Either functional or orther quantum chemical method
+                    for the density calculation. Default: 'PBE'
+                'basis_set': Basis set for the wavefunction description. 
+                    Default: 'def2-SVP'
+                'multiplicity': spin multiplicity of the system. Default: 1
+                'charge': charge of the system. Default: 0
+                'keywords': List of additional keywords that will be added to 
+                    after the '!' in the ORCA input file.
+                'blocks': everything that is included into the ORCA input file
+                    using a % sign. If a newline is present in the included 
+                    string an entry will be concluded with 'end' in the input 
+                    file otherwise a single line entry without end will be
+                    produced. If cluster charges are included, an existing
+                    'pointcharges' entry will be overwritten.
+        calc_options (Dict[str, Any]): Calculation options specific to the ORCA calculation. The dictionary should contain
+            keys such as 'filebase' and 'output_format'.
+    """
+    xyz_format = 'cartesian'
+    provides_output = ('mkl', 'wfn')
+
+    atom_site_required = (
+        '_atom_site_Cartn_x',
+        '_atom_site_Cartn_y',
+        '_atom_site_Cartn_z', 
+        '_atom_site_type_symbol'
+    )
+    
+    def __init__(self, *args, abs_orca_path: Optional[str] = None, **kwargs):
+        """
+        Initialize the ORCADensityCalculator instance.
+
+        Args:
+            *args: Variable length argument list.
+            abs_orca_path (Optional[str]): The absolute path of the ORCA 
+                executable. Defaults to None, in this case the absolute path 
+                is determined from an orca executable in PATH.
+            **kwargs: Arbitrary keyword arguments.
+        """
+
+        super().__init__(*args, **kwargs)
+        if abs_orca_path is not None:
+            self.abs_orca_path = abs_orca_path
+        elif platform.system() == 'Windows':
+            self.abs_orca_path = shutil.which('orca.exe')
+        elif platform.system() == 'Darwin':
+            self.abs_orca_path = shutil.which('orca')
+        else:
+            #assume linux
+            self.abs_orca_path = shutil.which('orca')
+
+    def check_availability(self) -> bool:
+        """
+        Check if the ORCA executable is available in the system.
+
+        Returns:
+            bool: True if the ORCA executable is available, False otherwise.
+        """
+        if self.abs_orca_path is not None:
+            path = pathlib.Path(self.abs_orca_path)
+            return path.exists()
+        else:
+            return False
+    
+
+    def calculate_density(
+            self,
+            atom_site_dict: Dict[str, Union[float, str]], 
+            cluster_charge_dict: Dict[str, List[float]] = {}
+        ):
+        """
+        Calculate the electronic density for a given atomic configuration using ORCA.
+
+        Args:
+            atom_site_dict (Dict[str, Union[float, str]]): Dictionary containing
+                the atomic configuration information.
+                Required keys: '_atom_site_type_symbol', '_atom_site_Cartn_x', 
+                '_atom_site_Cartn_y', '_atom_site_Cartn_z'
+            cluster_charge_dict (Dict[str, List[float]], optional): Dictionary 
+                containing cluster charge information. provide a n, 3 numpy 
+                array under 'positions' for the charge positions and a 
+                n sized array with the charges under 'charges'.
+                Defaults to an empty dict for no cluster charges.
+        """
+        missing = [entry for entry in self.atom_site_required if entry not in dict(atom_site_dict)]
+        if len(missing) > 0:
+            mis_str = ', '.join(missing)
+            raise ValueError(f'The following necessary entries are not in the atom_site_dict: {mis_str}')
+
+        # Merge defaults and user-supplied options
+        qm_options = qm_defaults.copy()
+        
+        #blocks and keyword merging needs to account for different cases
+        new_lower = [key.lower() for key in self.qm_options['keywords']]
+        keep_kw = list(kw for kw in qm_defaults['keywords'] if kw not in new_lower)
+
+        qm_opt_blocks = self.qm_options.get('blocks', {})
+        lower_keys = tuple(key.lower() for key in qm_opt_blocks.keys())
+        keep_blocks = {key: val for key, val in qm_defaults['blocks'].items()
+                        if key.lower() not in lower_keys}
+
+        qm_options.update(self.qm_options)
+        qm_options['keywords'] += keep_kw
+        qm_options['blocks'].update(keep_blocks)
+
+        calc_options = calc_defaults.copy()
+        calc_options.update(self.calc_options)
+
+        if len(cluster_charge_dict.get('charges', [])) > 0:
+            cc_file = self._generate_cluster_charge_file(cluster_charge_dict)
+            cc_filename = f"{calc_options['filebase']}.pc"
+            with open(cc_filename, 'w') as fo:
+                fo.write(cc_file)
+
+            qm_options['blocks']['pointcharges'] = f"{cc_filename}"
+ 
+        # Create the input file content
+        input_content = self._generate_orca_input(atom_site_dict, qm_options)
+
+        # Write the input file to disk
+        input_filename = f"{calc_options['filebase']}.inp"
+        with open(input_filename, 'w') as fo:
+            fo.write(input_content)
+
+        #Execute ORCA with the generated input file 
+        out_filename = f"{calc_options['filebase']}.out"
+        with open(out_filename, 'w') as fo:
+            subprocess.call(
+                [self.abs_orca_path, input_filename],
+                stdout=fo,
+                stderr=subprocess.STDOUT
+            )
+
+        format_standardise = calc_options['output_format'].lower().replace('.', '')
+        if  format_standardise == 'mkl':
+            subprocess.check_output(['orca_2mkl', calc_options['filebase']])
+        elif format_standardise == 'wfn':
+            subprocess.check_output(['orca_2aim', calc_options['filebase']])
+        else:
+            raise NotImplementedError('output_format from OrcaCalculator is not implemented. Choose either mkl or wfn')
+
+    def _generate_cluster_charge_file(
+            cluster_charge_dict: Dict[str, List[float]]
+        ) -> str:
+        """
+        Generate the content of the ORCA cluster charge file using the given cluster charge dictionary.
+
+        Args:
+            cluster_charge_dict (Dict[str, List[float]]): Dictionary containing cluster charge information.
+
+        Returns:
+            str: The content of the cluster charge file.
+        """
+        position_strings = iter(
+            ' '.join(f'{val: 12.8f}' for val in single_position)
+            for single_position in cluster_charge_dict['positions']
+        )
+
+        charge_block = '\n'.join(
+            f'{charge: 9.6f} {pos_string}' for charge, pos_string 
+            in zip(cluster_charge_dict['charges'], position_strings)
+        )
+        
+        return f"{len(cluster_charge_dict['charges'])}\n{charge_block}\n"
+
+    def _generate_orca_input(self, atom_site_dict: Dict[str, Union[float, str]], 
+                             qm_options: Dict[str, Union[str, int, float, List[str], Dict[str, str]]]) -> str:
+        """
+        Generate the content of the ORCA input file using the given atom_site_dict and qm_options.
+
+        Args:
+            atom_site_dict (Dict[str, Union[float, str]]): Dictionary containing the atomic configuration information.
+                Required keys: '_atom_site_type_symbol', '_atom_site_Cartn_x', '_atom_site_Cartn_y', '_atom_site_Cartn_z'
+            qm_options (Dict[str, Union[str, int, float, List[str], Dict[str, str]]]): Dictionary containing the quantum mechanics options.
+
+        Returns:
+            str: The content of the ORCA input file.
+        """
+          
+        # Set up the ORCA input file header
+        header = f"! {qm_options['method']} {qm_options['basis_set']}"
+
+        for entries in batched(qm_options['keywords'], 5):
+            header += '\n!' + ' '.join(entries)      
+
+        blocks = ''.join(
+            f'\n%{key}\n{entry}\nend\n' if ' ' in entry.strip() 
+            else f'\n%{key} {entry}\n'
+            for key, entry in qm_options['blocks'].items() 
+        )
+
+        charge_mult = f"*xyz {qm_options['charge']} {qm_options['multiplicity']}"
+
+        entries = [atom_site_dict.get[key] for key in (
+            '_atom_site_type_symbol',
+            '_atom_site_Cartn_x',
+            '_atom_site_Cartn_y',
+            '_atom_site_Cartn_z'
+        )]
+
+        # Generate the coordinates section
+        coordinates = [f"{element} {x} {y} {z}" for element, x, y, z in zip(*entries)]
+        coordinates_section = '\n'.join(coordinates)
+
+        # Combine sections into a complete input file
+        orca_input = f"{header}\n{blocks}\n{charge_mult}\n{coordinates_section}\n*\n"
+        return orca_input
+    
+    def cif_output(self) -> str:
+        # TODO: Implement the logic to generate a CIF output from the calculation
+        return 'Someone needs to implement this before production'
+    
+    def add_citation_strings(self) -> str:
+        # TODO: Add a short string with the citation as bib and a sentence what was done
+        return 'bib_string', 'sentence string'
+
+
+
+        
---
+++ scaff/LCAODensityPartitioners/HortonPartitioner.py
@@ -0,0 +1,159 @@
+from .LCAODensityPartitionerBase import LCAODensityPartitioner
+try:
+    import horton
+except:
+    horton = None
+    
+import numpy as np
+from contextlib import redirect_stdout
+from ..conversions import cell_dict2atom_sites_dict, create_hkl_dmin
+from ..util import batched
+from typing import Dict, Any, List, Optional
+
+defaults = {
+    'method': 'mbis',
+    'method_options': {},
+    'hkl_batch_size': 2000
+}
+
+class HortonPartitioner(LCAODensityPartitioner):
+    wpart = None
+
+    accepts_input = ('mkl', 'wfn')
+    
+    def __init__(self, options: Dict[str, Any] = {}):
+        """
+        Initialize HortonPartitioner with given options. Default options will be used if not provided.
+
+        Args:
+            options (Dict[str, Any], optional): Options for HortonPartitioner. Defaults to {}.
+        """
+        super().__init__()
+        for key, value in defaults.items():
+            if key not in options:
+                options[key] = value
+        
+        if options['method'].lower().startswith('hirshfeld'):
+            assert 'atomdb_path' in options, 'The Hirshfeld methods need a valid path to an an h5 file generated with horton-atomdb.py under the keyword "atomdb_path"'
+        self.options = options
+    
+    def check_availability(self) -> bool:
+        """
+        Check if the HORTON is available in the system.
+
+        Returns:
+            bool: True if HORTON is available, False otherwise.
+        """
+        return horton is not None
+    
+    def partition(self, density_path: str):
+        """
+        Perform partitioning using HORTON's methods given a path to the density 
+        data. 
+
+        Args:
+            density_path (str): Path to the density data file.
+        """
+        assert density_path is not None, 'So far density has not been partitioned, so a path is needed'       
+        mol = horton.IOData.from_file(density_path)
+        grid = horton.BeckeMolGrid(mol.coordinates, mol.numbers, mol.pseudo_numbers, mode='keep')
+        moldens = mol.obasis.compute_grid_density_dm(mol.get_dm_full(), grid.points)
+        
+        with open('horton.log', 'a') as fo:
+            with redirect_stdout(fo):
+                method = self.options['method']
+
+                if method.lower() == 'hirshfeld':
+                    atomdb = horton.ProAtomDB.from_file(self.options['atomdb_path'])
+                    wpart = horton.HirshfeldWPart(mol.coordinates, mol.numbers, mol.pseudo_numbers, grid, moldens, atomdb, **self.options['method_options'])
+                elif method.lower() == 'hirshfeld-i':
+                    atomdb = horton.ProAtomDB.from_file(self.options['atomdb_path'])
+                    wpart = horton.HirshfeldIWPart(mol.coordinates, mol.numbers, mol.pseudo_numbers, grid, moldens, atomdb, **self.options['method_options'])
+                elif method.lower() == 'iterative-stockholder':
+                    wpart = horton.IterativeStockholderWPart(mol.coordinates, mol.numbers, mol.pseudo_numbers, grid, moldens, **self.options['method_options'])
+                elif method.lower() == 'mbis':
+                    wpart = horton.MBISWPart(mol.coordinates, mol.numbers, mol.pseudo_numbers, grid, moldens, **self.options['method_options'])
+                else:
+                    raise NotImplementedError('Partitioning method not implemented. Use either Hirshfeld, Hirshfeld-I, Iterative-Stockholder or MBIS')
+                wpart.do_partitioning()
+                
+        self.wpart = wpart
+    
+    def calc_f0j(
+            self,
+            atom_indexes: List[int],
+            cell_dict: Dict[str, Any],
+            refln_dict: Dict[str, Any],
+            density_path: Optional[str] = None
+        ) -> np.ndarray:
+        """
+        Calculate atomic structure factors given atom indexes and cell dictionary.
+        Perform partitioning first if not done before. If the par
+
+        Args:
+            atom_indexes (List[int]): List of atom indexes.
+            cell_dict (Dict[str, Any]): Cell dictionary containing the unit cell
+                parameters in with the keys being the CIF format entries.
+            refln_dict (Dict[str, Any]): Miller indicees as refln_index keyed
+                lists or numpy arrays
+            density_path (Optional[str], optional): Path to the density data file. 
+                Defaults to None. Needed if partitioning was not done before
+
+        Returns:
+            np.ndarray: Calculated atomic structure factors.
+        """
+        if self.wpart is None:
+            self.partition(density_path)
+        
+        index_vec_h = np.array((
+            refln_dict['_refln_index_h'],
+            refln_dict['_refln_index_k'],
+            refln_dict['_refln_index_l']
+        )).T
+        
+        cell_mat_m = cell_dict2atom_sites_dict(cell_dict)['_atom_sites_Cartn_tran_matrix']
+        cell_mat_f = np.linalg.inv(cell_mat_m).T
+        f0j = np.zeros((len(atom_indexes), index_vec_h.shape[0]), dtype=np.complex128)
+        vec_s = np.einsum('xy, zy -> zx', cell_mat_f, index_vec_h)
+        for atom_index in atom_indexes:
+            print(atom_index, end='/ ')
+            at_grid = self.wpart.get_grid(atom_index)
+            coordinates = at_grid.points - at_grid.center
+            for f0j_index, vec_s_slice in enumerate(batched(vec_s, self.options['hkl_batch_size'])):
+                print(f0j_index, end=' ')
+                vec_s_array = np.array(vec_s_slice)
+                phase_factors = np.exp(2j * np.pi * np.einsum('ax, bx -> ab', vec_s_array, coordinates))
+                start = f0j_index * self.options['hkl_batch_size']
+                if vec_s_array.shape[0] == self.options['hkl_batch_size']:
+                    end = (f0j_index + 1) * self.options['hkl_batch_size']
+                else:
+                    end = f0j.shape[1]
+                f0j[atom_index, start:end] = np.sum(
+                    (at_grid.weights * self.wpart[('at_weights', atom_index)] * self.wpart.get_moldens(atom_index))[None,:] * phase_factors, 
+                    axis=1)
+            print('')
+        return f0j
+    
+    def calc_charges(self, atom_indexes: List[int], density_path: Optional[str] = None) -> np.ndarray:
+        """
+        Calculate atomic charges of given atom indexes. Perform partitioning 
+        first if not done before.
+
+        Args:
+            atom_indexes (List[int]): List of atom indexes.
+            density_path (Optional[str], optional): Path to the density data file. 
+                Defaults to None. Needed if partitioning was not done before
+
+        Returns:
+            np.ndarray: Calculated charges for each atom index.
+        """        
+        if self.wpart is None:
+            self.partition(density_path)
+            
+        return np.array([self.wpart['charges'][idx] for idx in atom_indexes])
+    
+
+    def add_citation_strings(self) -> str:
+        # TODO: Add a short string with the citation as bib and a sentence what was done
+        return 'bib_string', 'sentence string'
+    
