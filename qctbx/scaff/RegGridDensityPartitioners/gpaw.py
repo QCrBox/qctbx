Fix calculation of f0j_core

qctbx/scaff/RegGridDensityPartitioners/gpaw.py
@@ -171,11 +171,12 @@ class GPAWDensityPartitioner(RegGridDensityPartitioner):
         hdens_obj = HirshfeldDensity(calc)
         if options['partition'] == 'valence':
             skip_core = True
+            y00 = 0.5 * np.pi**(-0.5)
             splines = {setup.symbol: (setup.get_partial_waves()[2], setup.rgd.r_g) for setup in calc.density.setups}
             qubox_density_atomic_dicts = {
                 symbol: {
-                    '_qubox_density_atomic_rgrid': rgrid,
-                    '_qubox_density_atomic_core': spline.map(rgrid)
+                    '_qubox_density_atomic_rgrid': rgrid * ANGSTROM_PER_BOHR,
+                    '_qubox_density_atomic_core': spline.map(rgrid) / ANGSTROM_PER_BOHR**3 * y00
                 } for symbol, (spline, rgrid) in splines.items()
             }
             